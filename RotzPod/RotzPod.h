114ec8760ee54dfd818df47712ec0e7b2427bbdc
Dominik Liehr <dominik_liehr@me.com>
2018-10-25 21:06:12 +0200

    Initial Commit

---
+++ RotzPod/RotzPod.h
@@ -0,0 +1,19 @@
+//
+//  RotzPod.h
+//  RotzPod
+//
+//  Created by Dominik Liehr on 25.10.18.
+//  Copyright © 2018 Liehr IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RotzPod.
+FOUNDATION_EXPORT double RotzPodVersionNumber;
+
+//! Project version string for RotzPod.
+FOUNDATION_EXPORT const unsigned char RotzPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RotzPod/PublicHeader.h>
+
+
